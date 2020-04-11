6763cfbad135c1927dd7f8c478f0dcdc08103f55
Vicky Bajoria <vickybajoria@gmail.com>
2020-04-11 13:18:47 +0530

    Added, about us, products, contact us and home template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { Routes, RouterModule} from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'contact-us', component: ContactUsComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '**', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    ProductsComponent,
+    ContactUsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
